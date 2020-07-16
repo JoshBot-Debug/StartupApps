880257d43577c8b8ae9221a68da56364e71472fd
Joshua <joshuamyerswebdev@gmail.com>
2020-07-16 15:33:05 +0530

    took care of a small bug

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import fnmatch
+from pathlib import Path
 
 class Controller:
 
@@ -38,23 +39,29 @@ class Controller:
 
 
     def run(self):
-        with open(self.CurrentFile,"r",encoding="utf8") as config:
-            data = config.read().replace("\n"," ").split("-p")
-            print(data)
-            for i,row in enumerate(data):
-                if i > 0:
-                    if fnmatch.fnmatch(row, "*:/*") or fnmatch.fnmatch(row, "*:\\*"):
-                        correctedSlash = row.replace("/","\\")
-                        os.system("explorer "+correctedSlash)
-                    else:
-                        os.system(row)
+        if Path(self.CurrentFile).exists():
+            with open(self.CurrentFile,"r",encoding="utf8") as config:
+                data = config.read().replace("\n"," ").split("-p")
+                print(data)
+                for i,row in enumerate(data):
+                    if i > 0:
+                        if fnmatch.fnmatch(row, "*:/*") or fnmatch.fnmatch(row, "*:\\*"):
+                            correctedSlash = row.replace("/","\\")
+                            os.system("explorer "+correctedSlash)
+                        else:
+                            os.system(row)
+        self.help()
 
 
     def help(self):
         print("""
         [-h] => Help
         [-p] => Path to folder/executable/environment variable
+        
         To pass multiple files/folder/etc, seperate the paths by "-p".
 
         To execute the startup app, don't pass any paramenters
+
+        * make sure you have a StartupApps.config in the current directory,
+        if you do not, create one by passing in parameters to this app.
         """)
